fb327b89eb3777aae4458c0542bdffceb7d0b893
va-lang <vanessabedzra@gmail.com>
2023-12-26 11:48:19 -0800

    Header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**** MACROS ****/
+#define MAX_INPUT_SIZE 1024
+
+/* STANDARD LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif /* SHELL_H */
+
