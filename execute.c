f2f0ecab83c14168a2c7317b7bd6cafab82483d6
va-lang <vanessabedzra@gmail.com>
2024-01-03 10:58:43 -0800

    Committing local changes

--- execute.c
+++ execute.c
@@ -1,97 +1,165 @@
 #include "shell.h"
 
+
 /**
  * display_prompt - Displays the shell prompt
+ * Return: nothing is returned
  */
 
 void display_prompt(void)
 {
-    printf("($) ");
-    fflush(stdout);
+	printf("($) ");
 }
 
 /**
  * execute_command - Executes the given command
+ * @command: command to be executed
+ * @count: number of times a command has be passed to this program per session
+ * Return: nothing is returned
  */
-void execute_command(char *command)
+void execute_command(char *command, int count)
 {
-    if (strcmp(command, "exit") == 0)
-    {
-        printf("Exiting the shell.\n");
-        exit(EXIT_SUCCESS);
-    }
-    else
-    {
-        pid_t pid = fork();
+	char **argv;
+	int status=0;
+	pid_t pid;
+	char *exec_command;
+	char *path;
 
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            /* Child process */
+	if (strcmp(command, "exit") == 0)
+	{
+		if (command)
+		{
+			freepointer(command);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		path = getenv("PATH");
+		/*parse the command using whitespace*/
+		exec_command = processcommand(command, path);
+		if (exec_command != NULL)
+		{
+			argv = tokenizestr(exec_command, " ");
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+			/* child  process */
+				if (execve(argv[0], argv, NULL) == -1)
+				{
+					fprintf(stderr, "%s: %d: %s: %s\n", argv[0], count, command, "not found");
+					exit(errno);
+				}
+			}
+			else
+			{
+			freepointer(exec_command);
+			free_2dbuffer(argv);
+			/* Parent process */
+			parentprocess(pid, status, "./hsh");
+			}
 
-           char *args[2];
-            args[0] = command;
-            args[1] = NULL;
+		}
+		else
+		{
+			fprintf(stderr, "%s cannot be found\n", command);
+			/*return a status code*/
+		}
+	}
+}
 
-            if (execve(command, args, NULL) == -1)
-            {
-                perror(command);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent process */
+/**
+ *parentprocess - function to handle parent process
+ *@pid: pid of the process
+ *@status: status returned
+ *@command: command passed
+ *Return: nothing is returned
+ **/
 
-            int status;
-            if (waitpid(pid, &status, 0) == -1)
-            {
-                perror("waitpid");
-                exit(EXIT_FAILURE);
-            }
-
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-            {
-                fprintf(stderr, "./hsh: %s: command not found\n", command);
-            }
-            else if (WIFSIGNALED(status))
-            {
-                fprintf(stderr, "./hsh: %s: terminated by signal %d\n", command, WTERMSIG(status));
-            }
-        }
-    }
+void parentprocess(pid_t pid, int status, char *command)
+{
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		perror("waitpid");
+		exit(EXIT_FAILURE);
+	}
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+	{
+		fprintf(stderr, "./hsh: %s: %s\nstatus: %d, errno: %d\n", command, strerror(errno), status, errno);
+	}
+	else if (WIFSIGNALED(status))
+	{
+		fprintf(stderr, "./hsh: %s: terminated by signal %d\nstatus: %d errno: %d\n", command, WTERMSIG(status), status, errno);
+	}
 }
 
 /**
  * main - Entry point for the simple shell program
  * Return: Always 0 on success
  */
+
 int main(void)
 {
-    char *input = NULL;
-    size_t input_size = 0;
+	char *input = NULL;
+	size_t input_size = 0;
+	int count = 0;
 
-    while (1)
-    {
-        display_prompt();
+	while (1)
+	{
+		display_prompt();
 
-        if (getline(&input, &input_size, stdin) == -1)
-        {
-            printf("\n");
-            break;
-        }
+		if (getline(&input, &input_size, stdin) == -1)
+		{
+			if (input)
+			{
+				freepointer(input);
+			}
+			exit(EXIT_SUCCESS);
+		}
 
-        input[strcspn(input, "\n")] = '\0';
+		input[strcspn(input, "\n")] = '\0';
+		count++;
+		handlecommandline(input, count);
 
-        execute_command(input);
-    }
+	}
+	freepointer(input);
 
-    free(input);
-
-    return 0;
+	return (0);
 }
 
+/**
+ *handlecommandline - function to parse multiple commands
+ *@string: commands passed as strings
+ *@count: coun5 passed from the calling function
+ *return: no value is returned
+*/
+void handlecommandline(char *string, int count)
+{
+
+	char *argv[10];
+	size_t index = 0;
+	char *delim = ";&&";
+	char *token;
+
+	token = strtok(string, delim);
+
+	while (token != NULL)
+	{
+		argv[index] = token;
+		index += 1;
+		token = strtok(NULL, delim);
+	}
+	argv[index] = NULL;
+
+	index = 0;
+	while (argv[index] != NULL)
+	{
+		execute_command(argv[index], count);
+		index += 1;
+	}
+}
--- shell.h
+++ shell.h
@@ -3,6 +3,8 @@
 
 /**** MACROS ****/
 #define MAX_INPUT_SIZE 1024
+#define MAX_ARGUMENT 32
+#define BUFFER_SIZE 1024
 
 /* STANDARD LIBRARIES */
 #include <stdio.h>
@@ -11,6 +13,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <ctype.h>
+
+void display_prompt(void);
+void handlecommandline(char *string, int count);
+void freepointer(char *ptr);
+void execute_command(char *command, int count);
+void parentprocess(pid_t pid, int status, char *command);
+char *processcommand(char *string, char *path);
+char **tokenizestr(char *str, char *delim);
+void free_2dbuffer(char **args);
+char *handlewhitespace(char *str);
+char *get_full_path(const char *command, const char *path);
+char *parsepath(char *path_copy, char *input_command);
+void freepointer(char *ptr);
+char *extract_substring(const char *str);
+char *custom_getline(void);
 
 #endif /* SHELL_H */
-
